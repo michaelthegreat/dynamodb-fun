9093f083c8b8c952b30ff21bb73650fc0dcb9d3e
Michael Quintero <michaelq.quintero@gmail.com>
2025-08-19 11:52:18 -0700

    trim the fat on the requirements.txt

--- streamed-lambda-response/app.py
+++ streamed-lambda-response/app.py
@@ -25,7 +25,7 @@ def stream_pydantic_model(model: BaseModel, long_field: str, chunk_size: int = 1
         str: Chunks of JSON string
     """
     print(" in stream pydantic model")
-    data = model.dict()  # dict representation. todo: use model_dump
+    data = model.dict()  # dict representation. todo: use model_dump() if needed
     print("made it past model dump")
     if long_field not in data:
         raise ValueError(f"Field '{long_field}' not found in model")
@@ -58,35 +58,39 @@ def stream_pydantic_model(model: BaseModel, long_field: str, chunk_size: int = 1
 
     yield "\"}"
 
-def generate_large_json():
-    # Start of JSON object
-    yield '{'
-
-    # Stream the smaller fields
-    yield '"record_id": 1,'
-    yield '"attribute1": "abc",'
-
-    # Stream the long field in chunks
-    yield '"long_value": "'
-    long_value = "a very long string of text goes here " * 200000  # simulate ~10MB
-
-    chunk_size = 1024 * 64  # 64 KB per chunk
-    for i in range(0, len(long_value), chunk_size):
-        yield long_value[i:i+chunk_size]
-
-    yield '"}'  # close the string and object
-
-
-
 @app.route("/", methods=["GET"])
 def stream_large_json():
-    record = Record(
-        record_id=1,
-        attribute1="aabc",
-        long_value="a very long string of text here" * 200000
-    )
+    DEBUG_REQUEST = False
+    DEBUG_STREAM = True
+    if DEBUG_REQUEST:
+        print("request", request)
+        print("--------------------------")
+        print("dir(request )", dir(request))
+        print("--------------------------")
+        print("request environ", request.environ)
+        print("--------------------------")
+        print("request headers", request.headers)
+        print("===============================")
+        try:
+            print("lets goooo")
+            print("X-Amzn-Request-Context", request.headers["X-Amzn-Request-Context"])
+            print("X-amazn-Lambda-Context", request.headers["X-Amzn-Lambda-Context"])
+        except Exception as e:
+            print("Error accessing headers:", e)
 
-    return Response(
-        stream_with_context(stream_pydantic_model(record, "long_value")),
-        content_type="application/json"
-    )
+        return Response({
+            "message": "Hello, World!",
+        },content_type="application/json")
+    if DEBUG_STREAM:
+
+        record = Record(
+            record_id=1,
+            attribute1="aabc",
+            long_value="a very long string of text here" * 200000
+        )
+
+        return Response(
+            stream_with_context(stream_pydantic_model(record, "long_value")),
+            content_type="application/json"
+        )
+    # return generic hello world response
